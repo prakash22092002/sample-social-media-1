169cc038ffc28910912fbc1b7f9f1a1956d9f5ee
prakash <p.prakashpaudel35@gmail.com>
2024-10-21 21:41:17 +0530

    the post and get method using axios in realtime database -firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import React from "react";
+import { getDatabase } from "firebase/database";
+import firebaseApp, { databaseURL } from "./firebase/firebaseconfig";
+import "./App.css";
+import axios from "axios";
+const firebaseDatabase = getDatabase();
 
 function App() {
-  const [count, setCount] = useState(0)
+  async function writeDataToFirebase() {
+    try {
+      const response = await axios.post(`${databaseURL}/users.json`, {
+        userEmail: "p.prakashpaudel35@gmail.com",
+        userId: "4",
+        userName: "prakashPaudel",
+      });
+
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function readDataFromFirebase() {
+    try {
+      const response = await axios.get(`${databaseURL}/users.json`);
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={readDataFromFirebase}>READ DATA</button>
+      <button onClick={writeDataToFirebase}>WRITE DATA</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
